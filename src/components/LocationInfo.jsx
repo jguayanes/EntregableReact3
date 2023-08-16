7a85f0f8403eed6c5e3e5cec323ebc8737e82332
jguayanes <julianguayanes4@gmail.com>
2023-08-15 22:20:23 -0500

    ford commit

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -5,9 +5,9 @@ const LocationInfo = ({location}) => {
     <section className="flex flex-col gap-3 p-3 text-lg text-slate-500 items-center">
         <h2>{location?.name}</h2>
         <ul>
-            <li>Type:{location?.type}</li>
-            <li>Dimension: {location?.dimension}</li>
-            <li>Population: {location?.residents.length}</li>
+            <li><b className="text-white">Type: </b>{location?.type}</li>
+            <li><b className="text-white">Dimension: </b>{location?.dimension}</li>
+            <li><b className="text-white">Population:</b> {location?.residents.length}</li>
         </ul>
         <h1 className="grid justify-center text-[#82ff8b] text-[20px]">¡Wellcome to the crazy universe!</h1>
     </section>
