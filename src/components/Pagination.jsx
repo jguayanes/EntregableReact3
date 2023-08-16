ddaec98c13071b0b72484248bdb6771b5a93e6a7
jguayanes <julianguayanes4@gmail.com>
2023-08-15 21:21:12 -0500

    second commit

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,10 +2,10 @@
 
 const Pagination = ({pages, setCurrentPage, currentPage}) => {
     return (
-        <ul className="flex gap-5 text-2xl justify-center ">
+        <ul className="flex gap-5 text-2xl justify-center p-2 ">
             {
                 pages.map((page) => (
-                    <li className={`bg-[#062226]/60 grid items-center justify-center w-[60px] h-[60px] rounded-[6px] ${currentPage === pages && "hover:bg-[#062226]"}`} onClick={() => setCurrentPage(page)} key={page}>{page}</li>
+                    <li className={`bg-[#062226]/60 grid items-center justify-center w-[60px] h-[60px] rounded-[6px] cursor-pointer ${currentPage === pages && "hover:bg-[#062226]"}`} onClick={() => setCurrentPage(page)} key={page}>{page}</li>
                 ))
             }
         </ul>
